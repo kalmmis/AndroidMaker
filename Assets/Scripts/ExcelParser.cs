8bbd1db4b9b527b00ca355d3c70c1796e953fd49
kalmmis <basronyu@gmail.com>
2020-10-25 19:20:56 +0900

    dev: fixing UI resolution problem

--- Assets/Scripts/ExcelParser.cs
+++ Assets/Scripts/ExcelParser.cs
@@ -3,8 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
+
 public class ExcelParser : MonoBehaviour
 {
+    /*
+       static string m_resourcePath = "Assets/Data";
+
+       public static string GetResource(string type, int i)
+       {
+           string additionalPath = "level".Equals(type) ? "/level" : "/dialog";
+           FileStream f = new FileStream(m_resourcePath + additionalPath + i + ".csv", FileMode.Open, FileAccess.Read);
+           StreamReader reader = new StreamReader(f, System.Text.Encoding.UTF8);
+           string stageStr = reader.ReadToEnd();
+           return stageStr;
+       }
+   */
+
     static string m_resourcePath = "Assets/Data";
 
     public static string GetResource(string type, int i)
@@ -14,5 +28,71 @@ public class ExcelParser : MonoBehaviour
         StreamReader reader = new StreamReader(f, System.Text.Encoding.UTF8);
         string stageStr = reader.ReadToEnd();
         return stageStr;
+    }
+
+    public void writeStringToFile(string str, string filename)
+    {
+#if !WEB_BUILD
+        string path = pathForDocumentsFile(filename);
+        FileStream file = new FileStream(path, FileMode.Create, FileAccess.Write);
+
+        StreamWriter sw = new StreamWriter(file);
+        sw.WriteLine(str);
+
+        sw.Close();
+        file.Close();
+#endif
+    }
+
+    public string readStringFromFile(string filename)//, int lineindex)
+    {
+#if !WEB_BUILD
+        string path = pathForDocumentsFile(filename);
+
+        if (File.Exists(path))
+        {
+            FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(file);
+
+            string str = null;
+            str = sr.ReadLine();
+
+            sr.Close();
+            file.Close();
+
+            return str;
+        }
+
+        else
+        {
+            return null;
+        }
+#else
+return null;
+#endif
+    }
+
+    public string pathForDocumentsFile(string filename)
+    {
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            string path = Application.dataPath.Substring(0, Application.dataPath.Length - 5);
+            path = path.Substring(0, path.LastIndexOf('/'));
+            return Path.Combine(Path.Combine(path, "Documents"), filename);
+        }
+
+        else if (Application.platform == RuntimePlatform.Android)
+        {
+            string path = Application.persistentDataPath;
+            path = path.Substring(0, path.LastIndexOf('/'));
+            return Path.Combine(path, filename);
+        }
+
+        else
+        {
+            string path = Application.dataPath;
+            path = path.Substring(0, path.LastIndexOf('/'));
+            return Path.Combine(path, filename);
+        }
     }
 }
