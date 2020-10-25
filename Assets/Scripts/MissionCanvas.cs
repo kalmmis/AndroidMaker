807ee1fcf7ad3672a7d265d75e3813824edc4a0b
kalmmis <basronyu@gmail.com>
2020-10-25 15:44:18 +0900

    dev: android build set up
    bugs: 1) can't play dialogue part
    2) resolustion is failing

---
+++ Assets/Scripts/MissionCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionCanvas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    private void Init() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
