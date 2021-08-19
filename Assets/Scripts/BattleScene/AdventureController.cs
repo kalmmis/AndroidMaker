70633a0acdd274a44d7a2a7000006524620caebb
kalmmis <basronyu@gmail.com>
2021-08-20 00:43:56 +0900

    Update AdventureController.cs

--- Assets/Scripts/BattleScene/AdventureController.cs
+++ Assets/Scripts/BattleScene/AdventureController.cs
@@ -5,57 +5,38 @@ using UnityEngine.UI;
 
 public class AdventureController : MonoBehaviour
 {
-    private GameObject combatUI;
-    //[HideInInspector]
-    public GameObject resultPopUp;
     public Player player;
     public static bool isPlayerAlive = true;
-
-    public float invincibleTime = 3f;
-    //public string enemyType = "EnemyA";
     
     public Text ResultText;
     public static int curStageWave;
 
+    public GameObject resultPopUp;
     
     public void Start()
     {
         StartPlayer();
-    }
-    
-
-    public void StartPlayer()
-    {
-        StartCoroutine(InitPlayer(.1f));
-        //StartCoroutine(WaveEnemy(5.0f));
         curStageWave = 0;
         DoStage();
     }
 
-    public IEnumerator InitPlayer(float delayTime)
+    public void StartPlayer()
     {
         GameObject oldP = GameObject.Find("PlayerCube(Clone)");
-        //GameObject combatScreen;
-        yield return new WaitForSeconds(delayTime);
         if(oldP == null) { 
             Player p = Instantiate(player);
-            //RectTransform rectTransform = p.GetComponent<RectTransform>();
-            //rectTransform.anchoredPosition = new Vector2(-8,0);
             int lv = DataController.Instance.gameData.androidLv;
             List<Dictionary<string,object>> androidLevelInfo = CSVReader.Read ("AndroidLevelInfo");
             int tHp = (int)androidLevelInfo[lv]["HP"]; 
             p.hp = tHp;
             p.maxHp = tHp;
-            StartCoroutine(p.RemoveInvincible(invincibleTime));
         }
-        //애니메이팅 구현은 player 클래스 가서 해야 할 듯
-        //animator.SetTrigger("TrigPlayerIdle");
-        //playerShootingScript.TimeReset();
     }
-    
+
     public void DoStage()
     {
-        // Enemy.cs 에서 Destroy 타이밍에 count 를 세다가 전체 count 가 0이 되면 부름
+        // 좋은 구조는 아닌 것 같지만 DoStage 를 최초에 한번 부르고...
+        // Enemy.cs 에서 Destroy 타이밍에 count 를 세다가 전체 count 가 0이 되면 Wave ++ 로 계속 부름...
         List<Dictionary<string,object>> stageData = CSVReader.Read ("Stage02");
         string curWave = (string)stageData[curStageWave]["Wave"]; 
         StartCoroutine(SpawnWave(curWave));
@@ -79,7 +60,7 @@ public class AdventureController : MonoBehaviour
             //Debug.Log(enemyType);
 
             // 프리팹 파일명을 기준으로 적을 찾아서 인스턴시에이트해줌
-            var newEnemy = Instantiate(Resources.Load("Prefabs/" + enemyType), new Vector2(0, 0), Quaternion.identity) as GameObject;
+            var newEnemy = Instantiate(Resources.Load("Prefabs/Enemy/ " + enemyType), new Vector2(0, 0), Quaternion.identity) as GameObject;
             
             
             Enemy enemyScript = newEnemy.GetComponent<Enemy>();
@@ -95,15 +76,6 @@ public class AdventureController : MonoBehaviour
                 break;
             }
         }
-    }
-
-
-    public void LoadingAdventureUI()
-    {
-        combatUI = GameObject.FindGameObjectWithTag("CombatUI");
-        RectTransform rectTransform = combatUI.GetComponent<RectTransform>();
-        rectTransform.anchoredPosition = new Vector2(0,0);
-        //Win();
     }
 
     public void Win()
