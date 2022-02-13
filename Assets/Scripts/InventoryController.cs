5d038846d190b5ac44090b69eba8732a596df091
Karl <basronyu@gmail.com>
2022-02-13 18:39:36 +0900

    dev: 20220213
    인벤토리 표현 가능하게 구현 ㅠ

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -1,29 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryController : MonoBehaviour
 {
     private GameObject inventoryUI;
     private List<MyInventory> items;
+    private List<Dictionary<string,object>> itemInfo;
+    private int pageNumber = 2;
+    private Text itemSlotText;
 
     void Start()
     {
         inventoryUI = GameObject.FindGameObjectWithTag("InventoryUI");
         items = DataController.Instance.gameData.myInventoryList;
+        itemInfo = CSVReader.Read ("ItemInfo");
+        RefreshInventoryUI();
     }
 
     public void RefreshInventoryUI()
     {
+        Debug.Log ("How many kind items you have : " + items.Count);
+        //인벤토리에 존재하는 아이템 종류 수
+        int counter = 0;
+        int rowIndex = 0;
+        int slotIndex = 0;
+
         foreach (MyInventory item in items)
         {
-            
+            int id = item.id;
+            int amount = item.amount;
+            string itemName;
+
+            int pageIndex = (pageNumber - 1) * 12;
+            if(pageIndex <= counter && counter < pageIndex + 12)
+            {
+                itemName = (string)itemInfo[id]["itemName"];
+                
+                itemSlotText = inventoryUI.transform.Find("Panel").transform.Find("ItemScrollView").transform.Find("Viewport").transform.Find("Content").transform.Find("ItemPanelRow" + rowIndex.ToString()).transform.Find("ItemSlot" + slotIndex.ToString()).transform.Find("Text").GetComponent<Text>();
+                //미친 짓... 단순화할 방법을 찾아보자
+
+                itemSlotText.text = amount.ToString();
+                Debug.Log("you are having " + itemName);
+                Debug.Log("id is " + id + "/ amount is " + amount);
+                Debug.Log("slotIndex is " + slotIndex);
+                slotIndex++;
+            }
+            if (slotIndex == 4)
+            {
+                slotIndex = 0;
+                rowIndex++;
+            }
+            counter++;
         }
     }
 
     public void TestingAddInventory()
     {
-        AddItemToInventory(0,1);
+        AddItemToInventory(1,1);
     }
 
     public void TestingUseInventory()
